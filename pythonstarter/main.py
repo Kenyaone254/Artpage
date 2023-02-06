ef9862cdd35f64759e3703c22820c83c2513c218
kenyaone254 <ivyn8384@gmail.com>
2023-02-06 06:13:07 -0800

    updated

---
+++ pythonstarter/main.py
@@ -0,0 +1,37 @@
+print("Hello World")
+greeting = "Hello world"
+print(greeting)
+
+miles = 1000.0
+#casting simply changing or specifying the data of a variable
+print(int(miles))
+x = str(3)
+y = str(3)
+z = float(3)
+
+xy = '5'
+a = '5'
+c = int(xy) + int(a)
+
+print(c)
+# type() returns the type of a variable
+print(type(xy))
+
+Name1 = 'Joseph'
+name1 = 'Mary'
+print(name1) #Name1 will not overwrite name1
+# assign many values to mutiple values
+fruit1, fruit2, fruit3 = 'Orange', 'Banana', 'Cherry'
+print(fruit1)
+print(fruit2)
+print(fruit3)
+#one value to mutiple variables
+fruit4 = fruit5 = fruit6 = 'Apple'
+print(fruit4)
+print(fruit5)
+print(fruit6)
+fruits = 'Apple', 'cherry', 'banana'
+f1, f2, f3 = fruits
+print(f1)
+print(f2)
+print(f3)
